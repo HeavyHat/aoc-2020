594cf9788efc9351c76e8721529fda08b4b27655
Joshua Langley <Joshua.langley.1995@gmail.com>
2020-12-01 23:52:09 +0000

    Improvements to make it generic and without repeats.

--- exercises/1/main.cpp
+++ exercises/1/main.cpp
@@ -1,9 +1,106 @@
 #include <algorithm>
 #include <experimental/array>
+#include <vector>
 #include <iostream>
 #include <utility>
 
-static auto numbers = std::experimental::make_array(
+
+namespace detail {
+template <typename T, typename IntType, IntType... Idxs>
+auto get_tuple_of_size(std::integer_sequence<IntType, Idxs...>)
+    -> decltype(std::make_tuple((Idxs, std::declval<T>())...)) {
+  return {};
+}
+template <typename T, std::size_t N> auto get_tuple_of_size() {
+  return get_tuple_of_size<T>(std::make_integer_sequence<int, N>());
+}
+
+} // namespace detail
+
+template <typename T, std::size_t N>
+using XSumSolution = decltype(detail::get_tuple_of_size<T,N>()) ;
+
+namespace detail {
+
+
+template <typename... T, typename IntType, IntType... Idxs>
+    void print_helper(std::ostream& os, const std::tuple<T...>& solution, std::integer_sequence<IntType, Idxs...>) {
+        int dummy1[] = { ((os << (Idxs == 0 ? "" : " + ") << std::get<Idxs>(solution)), 0)... };
+        os << " = " << (std::get<Idxs>(solution) + ...) << std::endl;
+        int dummy2[] = { ((os << (Idxs == 0 ? "" : " X ") << std::get<Idxs>(solution)), 0)... };
+        os << " = " << (std::get<Idxs>(solution) * ...);
+    }
+}
+
+template <typename... T>
+inline std::ostream& operator<<(std::ostream& os, const std::optional<std::tuple<T...>>& solution) {
+    if(solution.has_value()) {
+        detail::print_helper(os, *solution, std::make_integer_sequence<int, sizeof...(T)>());
+    }
+    else {
+        os << "No " << sizeof...(T) << " sum solution could be determined.";
+    }
+    return os;
+}
+
+template <std::size_t X> struct XSum {
+  template <typename CONTAINER,
+            typename VALUE_TYPE = typename CONTAINER::value_type>
+  [[nodiscard]] auto find(VALUE_TYPE &&target, CONTAINER container) const
+      -> std::optional<XSumSolution<VALUE_TYPE, X>> {
+
+    std::unordered_map<int, int> map;
+    for (std::size_t i = 0; i < container.size() - X; ++i) {
+      auto complement = target - container.at(i);
+      auto maybeSubSum = XSum<X - 1>{}.find(complement, CONTAINER{std::next(container.begin(), i+2), container.end()});
+      if (maybeSubSum) {
+        return std::tuple_cat(std::make_tuple(container.at(i)), *maybeSubSum);
+      }
+    }
+    return std::nullopt;
+  }
+};
+
+template <> struct XSum<2> {
+  template <typename CONTAINER,
+            typename VALUE_TYPE = typename CONTAINER::value_type>
+  [[nodiscard]] auto find(VALUE_TYPE &&target, CONTAINER container) const
+      -> std::optional<XSumSolution<VALUE_TYPE, 2>> {
+
+    std::unordered_map<int, int> map;
+    for (std::size_t i = 0; i < container.size(); ++i) {
+      auto complement = target - container.at(i);
+      auto foundIt = map.find(complement);
+      if (foundIt != map.end()) {
+        return std::make_optional(
+            std::make_tuple(container.at(i), foundIt->first));
+      }
+      map[container.at(i)] = i;
+    }
+    return std::nullopt;
+  }
+};
+
+template <> struct XSum<0> {
+
+  template <typename CONTAINER,
+            typename VALUE_TYPE = typename CONTAINER::value_type>
+  auto find(int target, CONTAINER &&numbers) {
+    static_assert(sizeof(CONTAINER) == 0,
+                  "XSum only accepts values of 2 or more.");
+  }
+};
+template <> struct XSum<1> {
+
+  template <typename CONTAINER,
+            typename VALUE_TYPE = typename CONTAINER::value_type>
+  auto find(int target, CONTAINER &&numbers) {
+    static_assert(sizeof(CONTAINER) == 0,
+                  "XSum only accepts values of 2 or more.");
+  }
+};
+
+static std::vector<int> numbers{
     1664, 1909, 1904, 1638, 1844, 1836, 1648, 1710, 1163, 1684, 1857, 1257,
     1718, 1969, 1968, 1578, 1870, 1765, 1846, 1939, 1858, 1589, 1586, 1767,
     1628, 1595, 1601, 1528, 1724, 1656, 1555, 1150, 1992, 1380, 1142, 1615,
@@ -20,58 +117,19 @@ static auto numbers = std::experimental::make_array(
     1657, 1683, 1916, 1653, 1966, 82, 1102, 1535, 1748, 1609, 1996, 722, 1646,
     1167, 1784, 1616, 529, 1788, 1691, 1940, 1596, 1838, 1811, 1813, 1591, 1741,
     1606, 1871, 1997, 1827, 1492, 1789, 2002, 1702, 1876, 1251, 1237, 1510,
-    1093);
-
-template <typename T, std::size_t N>
-std::optional<std::pair<int, int>> find_2_sum(int target,
-                                              std::array<T, N> numbers) {
-  std::unordered_map<int, int> map;
-  for (std::size_t i = 0; i < numbers.size(); ++i) {
-    auto complement = target - numbers.at(i);
-    auto foundIt = map.find(complement);
-    if (foundIt != map.end()) {
-      return std::make_pair(foundIt->first, numbers.at(i));
-    }
-    map[numbers.at(i)] = i;
-  }
-  return std::nullopt;
-}
-
-template <typename T, std::size_t N>
-std::optional<std::tuple<int, int, int>> find_3_sum(int target,
-                                                    std::array<T, N> numbers) {
-  std::unordered_map<int, int> map;
-  for (std::size_t i = 0; i < numbers.size(); ++i) {
-    auto complement = target - numbers.at(i);
-    auto maybeTwoSum = find_2_sum(complement, numbers);
-    if (maybeTwoSum) {
-      return std::make_tuple(numbers.at(i), maybeTwoSum->first,
-                             maybeTwoSum->second);
-    }
-  }
-  return std::nullopt;
-}
+    1093};
 
 int main() {
   constexpr auto target = 2020;
   std::sort(numbers.begin(), numbers.end());
-  auto pair = find_2_sum(target, numbers);
-  if (pair.has_value()) {
-    std::cout << pair->first << " + " << pair->second << " = " << target
-              << std::endl;
-    std::cout << pair->first << " X " << pair->second << " = "
-              << (pair->first * pair->second) << std::endl;
-  } else {
-    std::cout << "No 2 sum solution is available.";
-  }
-  auto triplet = find_3_sum(target, numbers);
-  if (triplet.has_value()) {
-    auto [first, second, third] = *triplet;
-    std::cout << first << " + " << second << " + " << third << " = " << target
-              << std::endl;
-    std::cout << first << " X " << second << " X " << third << " = "
-              << (first * second * third) << std::endl;
-  } else {
-    std::cout << "No 3 sum solution is available.";
-  }
+  auto pair = XSum<2>{}.find(target, numbers);
+  std::cout << pair << std::endl;
+  auto triplet = XSum<3>{}.find(target, numbers);
+  std::cout << triplet << std::endl;
+  auto quad = XSum<4>{}.find(target, numbers);
+  std::cout << quad << std::endl;
+  auto quint = XSum<5>{}.find(target, numbers);
+  std::cout << quint << std::endl;
+  auto six = XSum<6>{}.find(target, numbers);
+  std::cout << six << std::endl;
 }
